12b84afd731ec32580ce9b43c2669ee105096f36
will <williamgeorge.barker@gmail.com>
2023-05-31 21:33:43 -0400

    added status

--- projects/jaffle_shop-will/models/staging/stripe/stg_payments.sql
+++ projects/jaffle_shop-will/models/staging/stripe/stg_payments.sql
@@ -10,6 +10,7 @@ renamed as (
         id as payment_id,
         orderid as order_id,
         paymentmethod as payment_method,
+        status,
 
         -- `amount` is currently stored in cents, so we convert it to dollars
         amount / 100 as amount,
