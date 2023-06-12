4223dcbfc4fcc4bf73c9a18ab6bca8850a60c36b
will <williamgeorge.barker@gmail.com>
2023-06-12 17:47:58 -0400

    refactor out intermediate logic

--- projects/jaffle_shop-will/models/marts/finance/fct_customers_orders.sql
+++ projects/jaffle_shop-will/models/marts/finance/fct_customers_orders.sql
@@ -8,19 +8,13 @@ with
 -- import ctes
 orders as (
 
-  select * from {{ ref('stg_jaffle_shop__orders') }}
+  select * from {{ ref('int_orders') }}
 
 ),
 
 customers as (
 
   select * from {{ ref('stg_jaffle_shop__customers') }}
-
-),
-
-payments as (
-
-  select * from {{ ref('stg_stripe__payments') }}
 
 ),
 
@@ -36,34 +30,25 @@ customer_order_history as (
 
     min(o.order_date) as first_order_date,
 
-    min(case
-      when o.status not in ('returned','return_pending')
-      then o.order_date
-    end) as first_non_returned_order_date,
+    min(o.valid_order_date) as first_non_returned_order_date,
 
-    max(case
-      when o.status not in ('returned','return_pending')
-      then o.order_date
-    end) as most_recent_non_returned_order_date,
+    max(o.valid_order_date) as most_recent_non_returned_order_date,
 
-    coalesce(max(user_order_seq),0) as order_count,
+    coalesce(max(o.user_order_seq),0) as order_count,
 
-    coalesce(count(case
-      when o.status != 'returned'
-      then 1
-    end),0) as non_returned_order_count,
+    coalesce(count(o.valid_order_date),0) as non_returned_order_count,
 
     sum(case
-      when o.status not in ('returned','return_pending')
-      then p.payment_amount else 0
+      when o.valid_order_date is not null
+      then o.order_value_dollars else 0
     end) as total_lifetime_value,
 
     sum(case
-      when o.status not in ('returned','return_pending')
-      then p.payment_amount else 0
+      when o.valid_order_date is not null
+      then o.order_value_dollars else 0
     end) /
     nullif(count(case
-      when o.status not in ('returned','return_pending')
+      when o.valid_order_date is not null
       then 1
     end),0) as avg_non_returned_order_value,
 
@@ -74,10 +59,7 @@ customer_order_history as (
   join customers c
   on o.customer_id = c.customer_id
 
-  left outer join payments p
-  on o.order_id = p.order_id
-
-  where o.status not in ('pending') and p.status != 'fail'
+  where o.order_status not in ('pending')
 
   group by c.customer_id, c.full_name, c.surname, c.givenname
 
@@ -93,23 +75,21 @@ final as (
     o.customer_id,
     coh.surname,
     coh.givenname,
-    p.payment_amount as order_value_dollars,
-    o.status as order_status,
-    p.status as payment_status,
+    o.order_value_dollars,
+    o.order_status,
+    o.payment_status,
 
     coh.first_order_date,
     coh.order_count,
     coh.total_lifetime_value,
+    coh.first_non_returned_order_date,
+    coh.most_recent_non_returned_order_date,
+    coh.non_returned_order_count,
 
   from orders o
 
   join customer_order_history coh
   on o.customer_id = coh.customer_id
-
-  left outer join payments p
-  on o.order_id = p.order_id
-
-  where p.status != 'fail'
 )
 
 -- simple select statement
