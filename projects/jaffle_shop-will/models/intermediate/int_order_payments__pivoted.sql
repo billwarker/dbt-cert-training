73b30b2390acf853519fec48418119629bfcf749
will <williamgeorge.barker@gmail.com>
2023-06-18 13:55:33 -0400

    create int_order_payments__pivoted with jinja templating

---
+++ projects/jaffle_shop-will/models/intermediate/int_order_payments__pivoted.sql
@@ -0,0 +1,27 @@
+{% set payment_methods = ['bank_transfer', 'credit_card', 'coupon', 'gift_card'] %}
+
+with payments as (
+    select * from {{ref('stg_stripe__payments')}}
+),
+
+payments_pivoted as (
+    select
+        order_id,
+
+        {% for method in payment_methods -%}
+
+        sum(case when payment_method = '{{ method }}' then payment_amount else 0 end) as {{method}}_amount
+
+        {%- if not loop.last -%}
+        ,
+        {% endif -%}
+        {% endfor %}
+    
+    from payments
+    where
+        status = 'success'
+    group by
+        1
+)
+
+select * from payments_pivoted
