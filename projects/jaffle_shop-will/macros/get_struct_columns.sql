d1bdd6460bbb67c4336ca97422a8a376bdb191f2
will <williamgeorge.barker@gmail.com>
2023-06-27 21:24:07 -0400

    formatting

--- projects/jaffle_shop-will/macros/get_struct_columns.sql
+++ projects/jaffle_shop-will/macros/get_struct_columns.sql
@@ -1,23 +1,23 @@
 {% macro get_struct_columns(relation, struct_col) %}
 
 {% set relation_query %}
-select
-    {{ struct_col }}
-from {{ relation }}
-limit 1
+    select
+        {{ struct_col }}
+    from {{ relation }}
+    limit 1
 {% endset %}
 
 {% set results = run_query(relation_query) %}
 
 {% if execute %}
 
-{% set results_list = results.columns[0].values() %}
-{% set results_dict = fromjson(results_list[0]) %}
-{% set output = results_dict.keys() | list %}
+    {% set results_list = results.columns[0].values() %}
+    {% set results_dict = fromjson(results_list[0]) %}
+    {% set output = results_dict.keys() | list %}
 
 {% else %}
 
-{% set output = [] %}
+    {% set output = [] %}
 
 {% endif %}
 
