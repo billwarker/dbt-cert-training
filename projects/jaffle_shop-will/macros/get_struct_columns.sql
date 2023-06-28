99def9c7b1d71bde6dd1b27726040f1a942936d5
will <williamgeorge.barker@gmail.com>
2023-06-27 21:23:25 -0400

    add my own get_struct_columns macro

---
+++ projects/jaffle_shop-will/macros/get_struct_columns.sql
@@ -0,0 +1,26 @@
+{% macro get_struct_columns(relation, struct_col) %}
+
+{% set relation_query %}
+select
+    {{ struct_col }}
+from {{ relation }}
+limit 1
+{% endset %}
+
+{% set results = run_query(relation_query) %}
+
+{% if execute %}
+
+{% set results_list = results.columns[0].values() %}
+{% set results_dict = fromjson(results_list[0]) %}
+{% set output = results_dict.keys() | list %}
+
+{% else %}
+
+{% set output = [] %}
+
+{% endif %}
+
+{{ return(output) }}
+
+{% endmacro %}
