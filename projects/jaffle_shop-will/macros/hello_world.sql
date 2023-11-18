145f2118c49b3fa27bde458ccde45ce2d3a5580d
will <williamgeorge.barker@gmail.com>
2023-11-18 18:32:50 -0500

    on-run-start macro

--- projects/jaffle_shop-will/macros/hello_world.sql
+++ projects/jaffle_shop-will/macros/hello_world.sql
@@ -1,5 +1,5 @@
 {% macro hello_world(name, subject = 'World') %}
 
-{{ log("My name is " ~ name ~ " and I say hello to the " ~ subject ~ " !!!", info=True) }}
+{{ log("My name is " ~ name ~ " and I say hello to the " ~ subject ~ "!!!", info=True) }}
 
 {% endmacro %}
